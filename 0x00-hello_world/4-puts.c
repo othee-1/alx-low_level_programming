947341cb57539cd61818ed276ebb8d52a89d68ca
othee-1 <thmnadms@gmail.com>
2022-09-11 09:29:05 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * Main - entry point
+ *
+ * mystring - stores string
+ *
+ * puts - prints  string to standard output
+ *
+ * return - returns  if successful.
+ */
+
+int main(void)
 {
-	char mystring[] = "Programming is like building a multilingual puzzle";
+	char mystring[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(mystring);
+
 	return (0);
 }
