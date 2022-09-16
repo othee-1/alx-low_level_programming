bb32ed8e86737d76620991e3bf8ea761904b9ef1
othee-1 <thmnadms@gmail.com>
2022-09-15 18:33:21 -0700

    another update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c = '0'; c < '9'; c++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(c);
+		_putchar(num + '0');
 	}
 
 	_putchar('\n');
