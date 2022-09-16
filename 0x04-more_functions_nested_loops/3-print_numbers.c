7f35853063d55577ea2c93920cc90573325eab49
othee-1 <thmnadms@gmail.com>
2022-09-15 18:17:27 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers 0 to 9.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num < '9'; c++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+}
